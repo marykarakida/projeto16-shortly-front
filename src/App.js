61ee480afe0210204abc8bae29fab424281955a3
Mary Karakida <makarakida@gmail.com>
2022-08-06 18:06:18 -0300

    feat: create Header and Navbar layout
    User can navigate between routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './assets/styles/reset.css';
 import './assets/styles/style.css';
 
+import { UserContextProvider } from './contexts/userContext';
+
 import PageView from './layout/PageView';
+import Header from './layout/Header';
 import HomePage from './pages/HomePage';
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/SignupPage';
@@ -12,16 +15,19 @@ import RankingPage from './pages/RankingPage';
 
 function App() {
     return (
-        <BrowserRouter>
-            <PageView>
-                <Routes>
-                    <Route path="/" element={<RankingPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/signin" element={<SigninPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                </Routes>
-            </PageView>
-        </BrowserRouter>
+        <UserContextProvider>
+            <BrowserRouter>
+                <PageView>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<RankingPage />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route path="/signin" element={<SigninPage />} />
+                        <Route path="/signup" element={<SignupPage />} />
+                    </Routes>
+                </PageView>
+            </BrowserRouter>
+        </UserContextProvider>
     );
 }
 
