fb690487b896c7efb33acb0032853a6df404547f
Mary Karakida <makarakida@gmail.com>
2022-08-07 10:43:17 -0300

    feat: create Signin Page layout

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageContainer from '../layout/PageContainer';
+import Form from '../components/Form/Form';
+import Input from '../components/Form/Input';
+import Button from '../components/Form/Button';
 
 export default function SigninPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <PageContainer>
-            <h1>This is Signin Page</h1>
+            <Form onSubmit={signIn}>
+                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" required />
+                <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Senha" required />
+                <Button type="submit">Entrar</Button>
+            </Form>
         </PageContainer>
     );
 }
