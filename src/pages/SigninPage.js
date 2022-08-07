ca05a65bfdc80716fd34da0b0145f9be531687f1
Mary Karakida <makarakida@gmail.com>
2022-08-07 10:58:19 -0300

    feat: add signin feature

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import useAxios from '../hooks/useAxios';
 
 import PageContainer from '../layout/PageContainer';
 import Form from '../components/Form/Form';
@@ -6,19 +9,50 @@ import Input from '../components/Form/Input';
 import Button from '../components/Form/Button';
 
 export default function SigninPage() {
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [{ loading, data }, executePost] = useAxios({ method: 'POST', route: '/signin' }, true);
+
+    useEffect(() => {
+        if (data) {
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('name', data.name);
+            navigate('/home', { replace: true });
+        }
+    }, [data]);
+
     const signIn = (e) => {
         e.preventDefault();
+        const body = { email, password };
+
+        executePost(body);
     };
 
     return (
         <PageContainer>
             <Form onSubmit={signIn}>
-                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" required />
-                <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Senha" required />
-                <Button type="submit">Entrar</Button>
+                <Input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={loading}
+                    placeholder="E-mail"
+                    required
+                />
+                <Input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled={loading}
+                    placeholder="Senha"
+                    required
+                />
+                <Button type="submit" disabled={loading}>
+                    Entrar
+                </Button>
             </Form>
         </PageContainer>
     );
